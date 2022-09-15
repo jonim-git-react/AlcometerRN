72bfab70944d91a4a9c17ee44b69b3a2431a1514
jonim-git <jonimaki1@gmail.com>
2022-09-15 18:35:17 +0300

    console logiin tekstiä kun weight arvo on null

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { Alert, Switch, Text, TextInput, Button, View, ScrollView } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import RadioForm  from 'react-native-simple-radio-button';
+import NumericInput from 'react-native-numeric-input';
+import RadioForm from 'react-native-simple-radio-button';
 import { DarkTheme, BrightTheme } from './MyStyles'
 
 export default function App() {
 
-  const [weight, setWeight] = useState(90)
+  const [weight, setWeight] = useState(null)
   const [bottles, setBottles] = useState(0)
   const [time, setTime] = useState(0)
   const [gender, setGender] = useState('male')
   const [result, setResult] = useState(0)
   const [isDarkTheme, setIsDarkTheme] = useState(false)
 
-  const theme = isDarkTheme? DarkTheme : BrightTheme;
+  const theme = isDarkTheme ? DarkTheme : BrightTheme;
 
   function changeTheme() {
-    setIsDarkTheme( prev => !prev )
+    setIsDarkTheme(prev => !prev)
     //tai lambdana ()=>setFancy(prev => !prev)}
   }
 
   function calculateBloodAlcoholLevel() {
 
+    if (weight !== null) {
     let grams = (bottles * 0.33) * 8 * 4.5
     let burning = weight / 10
     let gramsLeft = grams - (burning * time)
@@ -32,71 +34,63 @@ export default function App() {
     } else {
       result = gramsLeft / (weight * 0.6)
     }
+
+
     if (result < 0) {
-    setResult(0);
+      setResult(0);
     } else {
       setResult(result)
     }
+  } else {
+    console.log("input empty");
   }
+  
+}
 
-  function changeWeight(weight) {
-    setAge(weight);
-   
-  }
 
-  function changeBottles(bottles) {
-    setAge(bottles);
-    
-  }
+const genders = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+]
 
-  function changeTime(time) {
-    setAge(time);
-    
-  }
-
-  const genders = [
-    { label: 'Male', value: 'male' },
-    { label: 'Female', value: 'female' },
-  ]
-
-  return (
-    <View style={theme.container}>
-      <Text>Alcometer</Text>
-      <Text>Weight</Text>
-      <TextInput 
+return (
+  <ScrollView>
+    <Switch
+      // trackColor={{ false: "#767577", true: "#81b0ff" }}
+      // thumbColor={isD ? "#f5dd4b" : "#f4f3f4"}
+      onValueChange={changeTheme}
+      value={isDarkTheme}
+    />
+    <Text>Alcometer</Text>
+    <Text>Weight</Text>
+    <TextInput
+      name="weightInput"
       onChangeText={weight => setWeight(weight)}
       value={weight}
       keyboardType="numeric"
-      ></TextInput>
-      <Text>Bottles</Text>
-      <TextInput
-        onChangeText={bottles => setBottles(bottles)}
-        keyboardType={'number-pad'}
-      />
-      <Text>Time</Text>
-      <TextInput
-      onChangeText={time => setTime(time)}
+    />
+    <Text>Bottles</Text>
+    <NumericInput
+      minValue={0}
+      onChange={bottles => setBottles(bottles)}
       keyboardType={'number-pad'}
     />
-      {/* <DropDownPicker
-        items={intensities}
-        onSelect={(item, index) => setIntensity(item.value)}
-        value={intensity}
-        open={isOpen}
-        setOpen={() => setIsOpen(prev => !prev)}
-        setValue={i => setIntensity(i)}
-      /> */}
+    <Text>Time</Text>
+    <NumericInput
+      minValue={0}
+      onChange={time => setTime(time)}
+      keyboardType={'number-pad'}
+    />
+    <RadioForm
+      radio_props={genders}
+      initial={'0'}
+      onPress={value => setGender(value)}
+    />
 
-      <RadioForm
-        radio_props={genders}
-        initial={0}
-        onPress={value => setGender(value)}
-      />
+    <Text style={theme.label}>{result.toFixed(2)}</Text>
 
-      <Text>{result.toFixed(2)}</Text>
-
-      <Button title='CALCULATE' onPress={calculateBloodAlcoholLevel} />
-      <Button title='Change theme' onPress={changeTheme}></Button>
-    </View>
-  );
+    <Button title='CALCULATE' onPress={calculateBloodAlcoholLevel} />
+    <Button title='Change theme' onPress={changeTheme}></Button>
+  </ScrollView>
+);
 }
